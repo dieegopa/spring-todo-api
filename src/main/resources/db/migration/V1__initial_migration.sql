230802953d03b07257661983ab74bf749685c3c9
Diego Padilla <diegopadilla6@gmail.com>
2025-07-24 22:11:13 +0200

    first commit

---
+++ src/main/resources/db/migration/V1__initial_migration.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users
+(
+    id       BIGINT AUTO_INCREMENT NOT NULL,
+    name     VARCHAR(255) NOT NULL,
+    email    VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    CONSTRAINT `PRIMARY` PRIMARY KEY (id)
+);
